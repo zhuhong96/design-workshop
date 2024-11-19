cfe682d7fc91593754ef6f6b1d27c6aa8ded644f
zhuhong <zhuhong317626@163.com>
2024-11-19 21:02:29 +0800

    自动更新完成

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <span style="color: pink;">短时可获得集卡社</span>
-    <span style="#000">45646</span>
+    <div style="color: pink;">
+      成功了
+    </div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
