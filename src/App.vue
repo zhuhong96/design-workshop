d47d9d73aa91a5a2e12cd6546cf9c22e685b6fc3
zhuhong <zhuhong317626@163.com>
2024-11-19 20:21:53 +0800

    测试是否自动更新

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <span>45646</span>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
