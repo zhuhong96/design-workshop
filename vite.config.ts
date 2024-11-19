d8334db7eb039db77b28f18c52c570039cbd7ee6
chenyao <1574286558@qq.com>
2024-11-19 13:30:43 +0800

    :art: 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,46 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import viteCompression from 'vite-plugin-compression';
+import path, { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"./",
-  plugins: [vue()],
+  base: "./",
+  resolve:{
+    alias: {
+      '@': path.resolve('./src'), // 设置别名
+    }
+  },
+  plugins: [
+    vue(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    outDir: resolve(__dirname, 'dist'), // 指定输出路径
+    chunkSizeWarningLimit: 1500,
+    sourcemap: false, // 是否生成 source map
+    emptyOutDir: true, //Vite 会在构建时清空该目录
+    // 打包时清楚console和debugger
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        compact: true, //压缩代码，删除换行符等
+        assetFileNames: '[ext]/[name]-[hash].[ext]', //静态文件输出的文件夹名称
+        chunkFileNames: 'js/[name]-[hash].js', //chunk包输出的文件夹名称
+        entryFileNames: 'js/[name]-[hash].js', //入口文件输出的文件夹名称
+      },
+    },
+  }
 })
