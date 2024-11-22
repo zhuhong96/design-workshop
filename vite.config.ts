f3d7f4e152b7b844ba52d64b54d7b27d43e75188
雾恋 <zhuhong317626@163.com>
2024-11-22 17:31:45 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path, { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/design-workshop/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve('./src'), // 设置别名
